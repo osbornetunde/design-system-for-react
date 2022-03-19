d9e55dc3ec5d0cbf2974ddb67fabf864cfb2f81c
osbornetunde <osbornetunde@gmail.com>
2022-03-19 23:19:31 +0100

    fix(/.storybook/main.js): fixed the storybook build fail by moving the @storybook/preset-typescript

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  addons: [
-    "@storybook/preset-typescript",
-    "@storybook/addon-storysource",
-    "@storybook/addon-knobs",
-  ],
+  addons: ["@storybook/addon-storysource", "@storybook/addon-knobs"],
+  presets: ["@storybook/preset-typescript"],
   features: { postcss: false },
 };
